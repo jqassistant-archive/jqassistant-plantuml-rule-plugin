c1b68a6192778f86088fe16cff988ccda940cecf
Dirk Mahler <dirk.mahler@buschmais.com>
2018-05-02 22:07:31 +0200

    extract labels from nodes

--- src/main/java/org/jqassistant/contrib/plugin/plantumlrule/PlantUMLRulePlugin.java
+++ src/main/java/org/jqassistant/contrib/plugin/plantumlrule/PlantUMLRulePlugin.java
@@ -15,11 +15,14 @@ import com.buschmais.jqassistant.core.analysis.api.RuleLanguagePlugin;
 import com.buschmais.jqassistant.core.analysis.api.rule.ExecutableRule;
 import com.buschmais.jqassistant.core.analysis.api.rule.RuleException;
 import com.buschmais.jqassistant.core.analysis.api.rule.Severity;
-
 import com.buschmais.jqassistant.core.shared.asciidoc.AsciidoctorFactory;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Singular;
+import lombok.ToString;
 import net.sourceforge.plantuml.BlockUml;
 import net.sourceforge.plantuml.SourceStringReader;
-import net.sourceforge.plantuml.classdiagram.AbstractEntityDiagram;
 import net.sourceforge.plantuml.core.Diagram;
 import net.sourceforge.plantuml.cucadiagram.CucaDiagram;
 import net.sourceforge.plantuml.cucadiagram.ILeaf;
@@ -44,7 +47,7 @@ public class PlantUMLRulePlugin implements RuleLanguagePlugin {
 
     @Override
     public <T extends ExecutableRule<?>> Result<T> execute(T executableRule, Map<String, Object> ruleParameters, Severity severity, AnalyzerContext context)
-        throws RuleException {
+            throws RuleException {
         String diagramSource = getDiagramSource(executableRule);
         SourceStringReader reader = new SourceStringReader(diagramSource);
         List<BlockUml> blocks = reader.getBlocks();
@@ -56,16 +59,32 @@ public class PlantUMLRulePlugin implements RuleLanguagePlugin {
     }
 
     private <T extends ExecutableRule<?>> Result<T> evaluate(CucaDiagram diagram, T executableRule, Severity severity, AnalyzerContext context) {
+        Map<String, Node> nodes = getNodes(diagram);
+        List<Link> links = diagram.getLinks();
+        return new Result<>(executableRule, Result.Status.SUCCESS, severity, emptyList(), emptyList());
+    }
+
+    private Map<String, Node> getNodes(CucaDiagram diagram) {
         Collection<ILeaf> leaves = diagram.getLeafsvalues();
+        Map<String, Node> nodes = new HashMap<>();
         for (ILeaf leaf : leaves) {
-            String id = leaf.getUid();
+            Node.NodeBuilder nodeBuilder = Node.builder();
+            nodeBuilder.id(leaf.getUid());
             Stereotype stereotype = leaf.getStereotype();
+            for (String label : stereotype.getMultipleLabels()) {
+                if (label.startsWith("+")) {
+                    nodeBuilder.setLabel(label.substring(1));
+                } else {
+                    nodeBuilder.matchLabel(label);
+                }
+            }
+            Node node = nodeBuilder.build();
+            nodes.put(node.id, node);
             for (CharSequence charSequence : leaf.getDisplay()) {
                 // Extract attributes
             }
         }
-        List<Link> links = diagram.getLinks();
-        return new Result<>(executableRule, Result.Status.SUCCESS, severity, emptyList(), emptyList());
+        return nodes;
     }
 
     private <T extends ExecutableRule<?>> String getDiagramSource(T executableRule) throws RuleException {
@@ -92,5 +111,19 @@ public class PlantUMLRulePlugin implements RuleLanguagePlugin {
     private File getImagesDirectory(AbstractBlock abstractBlock) {
         String imagesDirectory = (String) abstractBlock.getDocument().getAttributes().get(AsciidoctorFactory.ATTRIBUTE_IMAGES_OUT_DIR);
         return new File(imagesDirectory);
+    }
+
+    @Builder
+    @Getter
+    @ToString
+    private static class Node {
+
+        private String id;
+
+        @Singular
+        private Set<String> matchLabels;
+
+        @Singular
+        private Set<String> setLabels;
     }
 }
